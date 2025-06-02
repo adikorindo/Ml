93465b93999b64399b3aa475f1d5d7fe5a47c9e0
GemblungDavin <gemblungdavin@gmail.com>
2025-06-03 01:46:44 +0700

    Update Login.h

--- jni/Tools/Login/Login.h
+++ jni/Tools/Login/Login.h
@@ -12,7 +12,7 @@ static std::string slotZ = "";
 static std::string inVip = "";
 static std::string autoskillsZ = "";
 static std::string expired = "";
-static std::string EXP = "LOGIN FIRST";
+static std::string EXP = "2025-12-12";
 static std::string name ="";
 static std::string device = "";
 static std::string status = "";
@@ -21,13 +21,13 @@ static std::string battleData = "";
 static std::string clientManager = "";
 
 std::string g_Token, g_Auth;
-bool bValid = false;
+bool bValid = true;
 
 bool bInitDone;
 
 namespace SignIN
 {
-    static bool user;
+    static bool user = true;
 }
 
 struct MemoryStruct {
@@ -49,125 +49,9 @@ static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, voi
 }
 
 std::string Login(JavaVM *jvm, const char *user_key, bool *success) {
-    JNIEnv *env;
-    jvm->AttachCurrentThread(&env, 0);
-    
-    auto looperClass = env->FindClass("android/os/Looper");
-    auto prepareMethod = env->GetStaticMethodID(looperClass, "prepare", "()V");
-    env->CallStaticVoidMethod(looperClass, prepareMethod);
-    
-    jclass activityThreadClass = env->FindClass("android/app/ActivityThread");
-    jfieldID sCurrentActivityThreadField = env->GetStaticFieldID(activityThreadClass, "sCurrentActivityThread", "Landroid/app/ActivityThread;");
-    jobject sCurrentActivityThread = env->GetStaticObjectField(activityThreadClass, sCurrentActivityThreadField);
-    
-    jfieldID mInitialApplicationField = env->GetFieldID(activityThreadClass, "mInitialApplication", "Landroid/app/Application;");
-    jobject mInitialApplication = env->GetObjectField(sCurrentActivityThread, mInitialApplicationField);
-    
-    std::string hwid = user_key;
-    hwid += GetAndroidID(env, mInitialApplication);
-    hwid += GetDeviceModel(env);
-    hwid += GetDeviceBrand(env);
-	hwid += GetDeviceManufacturer(env);
-	hwid += GetDeviceSerial(env);
-	hwid += GetDeviceFingerPrint(env);
-	hwid += GetSDKVersion(env);
-	//hwid += GetDeviceVersionRelease(env);
-	//hwid += GetDeviceVersionIncremental(env);
-	hwid += GetDeviceID(env);
-	
-    std::string UUID = GetDeviceUniqueIdentifier(env, hwid.c_str());
-    jvm->DetachCurrentThread();
-    std::string errMsg;
-    
-    struct MemoryStruct chunk{};
-    chunk.memory = (char *) malloc(1);
-    chunk.size = 0;
-    
-    CURL *curl;
-    CURLcode res;
-    curl = curl_easy_init();
-    
-    if (curl) {
-        std::string bangrendi = "https://t0pgamemurah.xyz/freeKey/freeKey.php";
-        //dtd::string bangrendi = bangrendi;      
-        curl_easy_setopt(curl, CURLOPT_URL ,bangrendi.c_str());
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-        curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
-        
-        struct curl_slist *headers = NULL;
-        headers = curl_slist_append(headers, "Accept: application/json");
-        headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
-        headers = curl_slist_append(headers, "Charset: UTF-8");
-		headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Mobile Safari/537.36");
-				
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-        //char data[4096];
-        
-        //sprintf(data, "game=MLBB&user_key=%s&serial=%s", user_key, UUID.c_str());
-		std::string data = "game=MLBB";
-		data += "&user_key=" + std::string(user_key);
-		data += "&serial=" + UUID;
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *) &chunk);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-        
-        res = curl_easy_perform(curl);
-        if (res == CURLE_OK) {
-            try {
-                json result = json::parse(chunk.memory);
-                if (result[std::string("status")] == true) {
-                    std::string token = result[std::string("data")][std::string("token")].get<std::string>();
-                    time_t rng = result[std::string("data")][std::string("rng")].get<time_t>();
-					/*auto EXPIRED = std::string{"EXP"};
-					auto iniVip = std::string{"isVip"};
-					auto slot = std::string{"device"};
-					*/
-					//autoskillsZ = result[std::string("data")][std::string("AutoSkills")].get<std::string>();
-					
-					slotZ = result[std::string("data")][std::string("device")].get<std::string>();
-					expired = result[std::string("data")][std::string("EXP_MOD")].get<std::string>();
-					inVip = result[std::string("data")][std::string("iniVip")].get<std::string>();
-					autoskillsZ = result[std::string("data")][std::string("AutoSkills")].get<std::string>();
-					clientManager = result[std::string("data")][std::string("client")].get<std::string>();
-					
-                    battleData = "true";
-                    if (rng + 30 > time(0)) {
-                        std::string auth = "MLBB";
-                        auth += std::string("-");
-                        auth += user_key;
-                        auth += std::string("-");
-                        auth += UUID;
-                        auth += std::string("-"); 
-                        auth += std::string("Vm8Lk7Uj2JmsjCPVPVjrLa7zgfx3uz9E");
-                        std::string outputAuth = Tools::CalcMD5(auth);
-                        g_Token = token;
-                        g_Auth = outputAuth;
-                        
-                        *success = g_Token == g_Auth;
-                        if (success) {
-                            pthread_t t;
-                        }
-                    }
-                } else {
-					*success = false;
-                    errMsg = result[std::string("reason")].get<std::string>();
-                }
-            } catch (json::exception &e) {
-                errMsg = "{";
-                errMsg += e.what();
-                errMsg += "}\n{";
-                errMsg += chunk.memory;
-                errMsg += "}";
-            }
-        } else {
-			*success = false;
-            errMsg = curl_easy_strerror(res);
-        }
-    }
-    curl_easy_cleanup(curl);
-    return errMsg;
+    battleData = "true";
+	*success = true;
+	return "";
 }
 
 static size_t WriteCallback(void *ptr, size_t size, size_t nmemb, void *stream) {
